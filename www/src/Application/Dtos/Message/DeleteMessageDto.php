34637dd2fcbcffca0bbd80120613cbb6f6e30212
Eric Neves <ericnevesrodrigues@gmail.com>
2024-08-10 14:52:04 -0400

    Refactor: refactor delete message

--- www/src/Application/Dtos/Message/DeleteMessageDto.php
+++ www/src/Application/Dtos/Message/DeleteMessageDto.php
@@ -4,7 +4,7 @@ namespace App\Application\Dtos\Message;
 
 class DeleteMessageDto
 {
-    public function __construct(public string $user_id, public string $secret_key)
+    public function __construct(public string $user_id)
     {
     }
 }
