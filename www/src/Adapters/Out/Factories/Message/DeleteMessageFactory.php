f26a4b8034a1ef88b9a89668ea75bb3a6e258313
Eric Neves <ericnevesrodrigues@gmail.com>
2024-08-11 13:10:32 -0400

    Refactor: remove service unused

--- www/src/Adapters/Out/Factories/Message/DeleteMessageFactory.php
+++ www/src/Adapters/Out/Factories/Message/DeleteMessageFactory.php
@@ -4,8 +4,6 @@ namespace App\Adapters\Out\Factories\Message;
 
 use App\Adapters\Out\Persistence\DAOs\MessageRedisDAO;
 use App\Adapters\Out\Persistence\Repositories\MessageRedisRepository;
-use App\Adapters\Out\Services\PasswordHasherImplementation;
-use App\Application\Services\MessageService;
 use App\Application\UseCases\Message\DeleteMessage\DeleteMessageUseCase;
 use App\Infrastructure\Database\Redis;
 
@@ -13,11 +11,9 @@ class DeleteMessageFactory
 {
     public function init(): DeleteMessageUseCase
     {
-        $messageRedisDAO              = new MessageRedisDAO(Redis::connect());
-        $messageRedisRepository       = new MessageRedisRepository($messageRedisDAO);
-        $passwordHasherImplementation = new PasswordHasherImplementation();
-        $messageService               = new MessageService($messageRedisRepository, $passwordHasherImplementation);
-        $deleteMessageUseCase         = new DeleteMessageUseCase($messageRedisRepository, $messageService);
+        $messageRedisDAO        = new MessageRedisDAO(Redis::connect());
+        $messageRedisRepository = new MessageRedisRepository($messageRedisDAO);
+        $deleteMessageUseCase   = new DeleteMessageUseCase($messageRedisRepository);
 
         return $deleteMessageUseCase;
     }
